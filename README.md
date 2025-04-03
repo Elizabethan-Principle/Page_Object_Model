d0a305d4e40b35ada0777bf1dd6e7004a34986ea
Elizabethan-Principle <elizabethezirim01@gmail.com>
2025-04-03 17:14:54 +0100

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Page_Object_Model
+
+This is for Page Object Model Structure
---
+++ SauceDemo/LocatorsCollections/LandingPagePOM/landingPageObjects.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
+
+
+class loginPage:
+    USERNAME = (By.ID, '')
+    PASSWORD = (By.CSS_SELECTOR, "")
+    LOGIN = (By.XPATH, "")
+
+
+
